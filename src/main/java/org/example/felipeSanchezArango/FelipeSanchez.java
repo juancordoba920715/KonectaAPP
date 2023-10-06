14c2bb66bc73c0eaed6dbde066dc86c25696ac5b
hfelipesa <hfelipesa@hotmail.com>
2023-10-06 16:40:23 -0500

    Creando package y clase propia

---
+++ src/main/java/org/example/felipeSanchezArango/FelipeSanchez.java
@@ -0,0 +1,8 @@
+package org.example.felipeSanchezArango;
+
+public class FelipeSanchez {
+        static String name="Hector Felipe Sanchez Arango";
+
+        public String notas="Me reuni con los campañeros y realice validacion usando las condiciones y validaciones";
+
+}
