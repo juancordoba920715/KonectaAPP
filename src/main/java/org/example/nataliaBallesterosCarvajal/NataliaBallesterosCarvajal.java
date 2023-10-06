9bfb19004bdd8bdd429f2ad5a905b027dcbf8f8f
Nata <talla0595@hotmail.com>
2023-10-06 16:44:13 -0500

    se creo un paquete, clase

---
+++ src/main/java/org/example/nataliaBallesterosCarvajal/NataliaBallesterosCarvajal.java
@@ -0,0 +1,7 @@
+package org.example.nataliaBallesterosCarvajal;
+
+public class NataliaBallesterosCarvajal {
+    /* Yo entendi el concepto de validaciones, pero las validaciones de esta actividad fueron hechas por
+      los compañeros que necesitaban practicar e interiorizar mas el tema, yo solo di mi opinion y si lo
+       que estaba haciendose estaba bien hecho  */
+}
