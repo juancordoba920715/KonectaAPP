cdb393650e88516db7710a3232470c8743055338
AndresFlorez <correo@gmail.com>
2023-10-06 16:44:16 -0500

    se creo clase con un comentario

---
+++ src/main/java/org/example/andresFlorez/AndresFlorez.java
@@ -0,0 +1,10 @@
+package org.example.andresFlorez;
+
+public class AndresFlorez {
+    /*
+    No he realizado ningun aporte importante, he estado como espetador, todabia me cuesta entender los conceptos objeto, metodo, matches, patches, y lo otro es que no encuentro click derecho cancelar curso.
+
+
+
+    */
+}
